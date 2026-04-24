93d2a3f63b4ee7c35ebf6ef8534b8eed47f4c5a8
jaberkro <jaberkro@student.codam.nl>
2026-04-24 12:57:42 +0200

    Exercise 00 working

---
+++ Python-0-Starting/ex00/Hello.py
@@ -0,0 +1,17 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = ("Hello", "The Netherlands!")
+ft_set.discard("Hello")
+ft_set.discard("tutu!")
+ft_set.add("Amsterdam!")
+ft_set.add("Hello")
+ft_dict["Hello"] = "Codam!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
