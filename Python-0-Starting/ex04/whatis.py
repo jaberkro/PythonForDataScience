8e0af54cf68afa8ce43993ab0175dbadd2153fdb
jaberkro <jaberkro@student.codam.nl>
2026-05-15 20:20:45 +0200

    ex04 built and tested

---
+++ Python-0-Starting/ex04/whatis.py
@@ -0,0 +1,22 @@
+import sys
+
+try:
+    assert len(sys.argv) > 1, ""
+except AssertionError:
+    sys.exit()
+
+try:
+    assert len(sys.argv) == 2, "more than one argument is provided"
+
+    try:
+        argument = int(sys.argv[1])
+        
+        if argument % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except:
+        raise AssertionError("argument is not an integer")
+except AssertionError as error:
+    print("AssertionError:", error.args[0])
+    
