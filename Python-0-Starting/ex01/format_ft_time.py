9896dd5df2492c9ebe0d4d0a4df3ed17cf2bc0f5
jaberkro <jaberkro@student.codam.nl>
2026-05-24 08:38:00 +0200

    breaking line that is too long into 2 lines, adding newline at end of file

--- Python-0-Starting/ex01/format_ft_time.py
+++ Python-0-Starting/ex01/format_ft_time.py
@@ -1,4 +1,5 @@
 import time
 
-print(f"Seconds since January 1, 1970: {time.time():,.4f} or {time.time():.2e} in scientific notation")
+print(f"Seconds since January 1, 1970: {time.time():,.4f} or \
+    {time.time():.2e} in scientific notation")
 print(time.strftime("%b %d %Y", time.localtime()))
