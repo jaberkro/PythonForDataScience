6b0d36e43f3b47c96e674d158afa9ad3f81e794f
jaberkro <jaberkro@student.codam.nl>
2026-05-24 14:08:15 +0200

    working

---
+++ Python-0-Starting/ex07/sos.py
@@ -0,0 +1,68 @@
+import sys
+
+
+def getNestedMorse():
+    NESTED_MORSE = {" ": "/ ",
+                    "A": ".- ",
+                    "B": "-... ",
+                    "C": "-.-. ",
+                    "D": "-.. ",
+                    "E": ". ",
+                    "F": "..-. ",
+                    "G": "--. ",
+                    "H": ".... ",
+                    "I": ".. ",
+                    "J": ".--- ",
+                    "K": "-.- ",
+                    "L": ".-.. ",
+                    "M": "-- ",
+                    "N": "-. ",
+                    "O": "--- ",
+                    "P": ".--. ",
+                    "Q": "--.- ",
+                    "R": ".-. ",
+                    "S": "... ",
+                    "T": "- ",
+                    "U": "..- ",
+                    "V": "...- ",
+                    "W": ".-- ",
+                    "X": "-..- ",
+                    "Y": "-.-- ",
+                    "Z": "--.. ",
+
+                    "0": "----- ",
+                    "1": ".---- ",
+                    "2": "..--- ",
+                    "3": "...-- ",
+                    "4": "....- ",
+                    "5": "..... ",
+                    "6": "-.... ",
+                    "7": "--... ",
+                    "8": "---.. ",
+                    "9": "----. "
+                    }
+
+    return NESTED_MORSE
+
+
+def main():
+    try:
+        nestedMorse = getNestedMorse()
+        morseOutput = ""
+
+        assert len(sys.argv) == 2, "the arguments are bad"
+
+        for char in sys.argv[1]:
+            assert char.isspace() or char.isalnum(), "the arguments are bad"
+
+        for char in sys.argv[1]:
+            morseOutput += nestedMorse[char.upper()]
+        print(morseOutput[:-1])
+
+    except AssertionError as error:
+        print("AssertionError:", error.args[0])
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
