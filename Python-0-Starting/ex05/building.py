8c20b06be021abd2b6f989f0f97d38bb8cd5b304
jaberkro <jaberkro@student.codam.nl>
2026-05-24 09:47:51 +0200

    docstrings added

--- Python-0-Starting/ex05/building.py
+++ Python-0-Starting/ex05/building.py
@@ -2,6 +2,7 @@ import sys
 
 
 def countText(textToCount):
+    """Prints the total count of characters and count per type for a string"""
     print("The text contains", len(textToCount), "characters")
     print(sum(1 for x in textToCount if x.isupper()), "upper letters")
     print(sum(1 for x in textToCount if x.islower()), "lower letters")
@@ -17,6 +18,8 @@ def countText(textToCount):
 
 
 def getUserInput():
+    """Takes the input argument from the user, or prompts the user to \
+        give one if it was not provided or empty"""
     userInput = ""
 
     if len(sys.argv) == 2:
@@ -30,6 +33,8 @@ def getUserInput():
 
 
 def main():
+    """Program to print the total character count of an inputted string, \
+        as well as the count per type of characters"""
     try:
         assert len(sys.argv) < 3, "more than one argument is provided"
     except AssertionError as error:
