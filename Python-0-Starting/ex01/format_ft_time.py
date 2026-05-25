15dff316dfa1ad72fac9be1279d08bc254fd2596
jaberkro <jaberkro@student.codam.nl>
2026-05-25 13:20:19 +0200

    removed tab to match expected output printed in terminal

--- Python-0-Starting/ex01/format_ft_time.py
+++ Python-0-Starting/ex01/format_ft_time.py
@@ -3,5 +3,5 @@ from time import strftime
 from time import localtime
 
 print(f"Seconds since January 1, 1970: {time():,.4f} or \
-    {time():.2e} in scientific notation")
+{time():.2e} in scientific notation")
 print(strftime("%b %d %Y", localtime()))
